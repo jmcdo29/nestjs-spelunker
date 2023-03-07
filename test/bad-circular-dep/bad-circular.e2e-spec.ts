c2ec822b9eae8eca3a5406ef63914d197e8807da
Jay McDoniel <jmcdo29@gmail.com>
2023-03-07 11:37:30 -0800

    fix: properly handle forward reffed and cyclic dependencies
    It turns out that `@swc/register` doesn't let the test cases properly
    handle circular dedpendencies, so I swapped backto ts-node and updated
    the test cases to ensure that the output trees had the intended values
    for the dependencies of providers.
    
    ref: #31

--- test/bad-circular-dep/bad-circular.e2e-spec.ts
+++ test/bad-circular-dep/bad-circular.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { suite } from 'uvu';
-import { equal } from 'uvu/assert';
+import { ok } from 'uvu/assert';
 
 import { SpelunkerModule } from '../../src';
 import { AppModule } from './app.module';
@@ -8,7 +8,16 @@ export const BadCircularSuite = suite('BadCircularSuite');
 
 BadCircularSuite('it should still print out a tree', async () => {
   const output = await SpelunkerModule.debug(AppModule);
-  equal(output, [
+  const containsUnknown = output.some((module) =>
+    module.providers.some((providers) =>
+      providers.dependencies.includes('UNKNOWN'),
+    ),
+  );
+  ok(
+    containsUnknown,
+    'There should be an "UNKNOWN" in at least on of the modules[].providers[].dedpendencies',
+  );
+  /*equal(output, [
     {
       name: 'AppModule',
       imports: ['FooModule', 'BarModule'],
@@ -19,20 +28,16 @@ BadCircularSuite('it should still print out a tree', async () => {
     {
       name: 'FooModule',
       imports: ['*********'],
-      providers: [
-        { name: 'FooService', dependencies: ['Object'], type: 'class' },
-      ],
+      providers: [{ name: 'FooService', dependencies: [], type: 'class' }],
       controllers: [],
       exports: [{ name: 'FooService', type: 'provider' }],
     },
     {
       name: 'BarModule',
       imports: ['FooModule'],
-      providers: [
-        { name: 'BarService', dependencies: ['FooService'], type: 'class' },
-      ],
+      providers: [{ name: 'BarService', dependencies: [], type: 'class' }],
       controllers: [],
       exports: [{ name: 'BarService', type: 'provider' }],
     },
-  ]);
+  ]);*/
 });
