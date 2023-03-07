bd7810b6c1f8eaef556e47b77094aa6f0fb20aac
Jay McDoniel <jmcdo29@gmail.com>
2023-03-06 17:39:07 -0800

    chore: remove swc to use ts-node instead
    Turns out, swc will cause some false positives when working with
    circular dependencies, so ts-node needs to be used instead to be
    as close to regular node as possible

--- test/good-circular-dep/bad-circular-dep/foo.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-import { BarService } from './bar.service';
-
-@Injectable()
-export class FooService {
-  constructor(private readonly bar: BarService) {}
-}
